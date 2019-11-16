c4e8e8df39ba3813770142a4ef0c70f58c525788
Zain Mansoor <zainmansoor77@gmail.com>
2019-11-16 23:27:37 +0500

    Add files via upload

---
+++ liveStream_video_recording.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+#print("Hello, Worldd!")
+'''import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd'''
+
+import cv2
+cap = cv2.VideoCapture(0)
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
+out = cv2.VideoWriter('output.mp4', fourcc, 20.0, (640,480))
+while (cap.isOpened()):
+    ret, frame = cap.read()
+    out.write(frame)
+    gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    
+    cv2.imshow("frame",gray)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+    
+cap.release()
+out.release()
+cv2.destroyAllWindows()    
